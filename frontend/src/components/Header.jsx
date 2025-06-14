3c32fba1c17730507b90412c2756835a9d4578cd
Jason <jasondoroughonfb@live.com>
2025-06-13 22:52:50 -0700

    Third commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { FaShoppingCart, FaUser } from 'react-icons/fa';
 const Header = () => {
     return (
         <header>Header
-            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+            <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>
                 <Container>
                     <Navbar.Brand href='/'>ProShop</Navbar.Brand>
                     <Navbar.Toggle aria-controls='basic-navbar-nav'></Navbar.Toggle>
