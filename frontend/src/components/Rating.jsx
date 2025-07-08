666e52503f656ed9570bb815257cf546c990f941
Jason <jasondoroughonfb@live.com>
2025-07-08 04:32:16 -0700

    Eighth (Rating Part 1) commit

---
+++ frontend/src/components/Rating.jsx
@@ -0,0 +1,13 @@
+
+import {FaStar, FaStarHalfAlt, FaRegStart} 'react-icons/fa';
+import React from 'react'
+
+const Rating = ({ value, text }) => {
+  return (
+    <div className='rating'>
+
+    </div>
+  )
+}
+
+export default Rating;
