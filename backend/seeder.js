1ee74bd799ece63087a8d5f8ccb3a5d27fb21dbd
Jason <jasondoroughonfb@live.com>
2025-07-21 23:50:19 -0700

    Nineteenth (Postman Setup) commit

--- backend/seeder.js
+++ backend/seeder.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import colors from "colors";
-import users from "./data/users.js"; // full name needed
+import users from "./data/users.js"; // full name req.
 import products from "./data/products.js";
 import User from "./models/userModel.js";
 import Product from "./models/productModel.js";
