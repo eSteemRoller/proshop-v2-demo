41cbf93af434149d5e28e92f5a6461304cea3d45
Jason <jasondoroughonfb@live.com>
2025-07-28 22:38:43 -0700

    Twentysecond (error handlers, controllers) commit

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,32 +1,12 @@
 
 import express from "express";
 const router = express.Router();
-// import products from '../data/products.js'; // local server data
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
-// import { message } from "statuses";
+// import products from '../data/products.js'; // original local pseudo-server data file
+import { getProducts, getProductById } from "../controllers/productController.js";
 
+router.route('/').get(getProducts);
 
-router.get(
-    '/', 
-    asyncHandler(async (req, res) => {
-        console.log("GET /api/products hit");
-        const products = await Product.find({});
-        res.json(products);
-    })
-);
-
-router.get(
-    '/:id', // Links to server.js: app.use
-    asyncHandler(async (req, res) => {
-        const product = await Product.findById(req.params.id);
-
-        if (product) {
-            return res.json(product);
-        }
-        res.status(404).json({ message: 'Product not found' });
-    })
-);
+router.route('/:id').get(getProductById);
 
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorHandlers.js';
 import productRoutes from './routes/productRoutes.js';
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 connectDB(); // Connect to MongoDB
 
@@ -19,5 +20,7 @@ app.get('/', (req, res) => {
 
 app.use('/api/products', productRoutes); // Links to productRoutes.js: router.get
 
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
