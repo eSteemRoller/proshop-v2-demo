cce19498d0b4d3583115dbaa4cf33ba6d0d3392d
Jason <jasondoroughonfb@live.com>
2025-08-12 23:00:26 -0700

    Thirtyfourth (userController, userRoutes, Part 2, Postman setup) commit

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,63 +7,70 @@ import User from "../models/userModel.js";
 // @route  POST /api/users/login
 // @access  Public
 const authUser = asyncHandler(async (req, res) => {
-  res.send('(POST) Auth User');
+  res.send('(POST) Public - Auth User');
 });
 
 // @desc  Create/Signup/Register user
 // @route  POST /api/users
 // @access  Public
 const signupUser = asyncHandler(async (req, res) => {
-  res.send('(POST) Signup (Create) User');
+  res.send('(POST) Public - Signup (Create) user');
 });
 
 // @desc  Logout user & clear (delete) cookie
 // @route  POST /api/users/logout
 // @access  Private (User)
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send('(POST) Logout User');
+  res.send('(POST) User - Logout User');
 });
 
 // @desc  Read/Get user profile
-// @route  GET /api/users/userProfile
+// @route  GET /api/users/user
 // @access  Private (User)
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.send('(GET) Read User (self) profile');
+  res.send('(GET) User - Read User (self) profile');
 });
 
 // @desc  Update user profile
-// @route  PUT /api/users/userProfile
+// @route  PUT /api/users/user
 // @access  Private (User)
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.send('(PUT) Update User (self) profile');
+  res.send('(PUT) User - Update User (self) profile');
+});
+
+// @desc  Create/Signup/Register user
+// @route  POST /api/users
+// @access  Private (Admin)
+const createUser = asyncHandler(async (req, res) => {
+  res.send('(POST) Admin - Create user');
 });
 
 // @desc  Get/Read all users
 // @route  GET /api/users
 // @access  Private (Admin)
 const getAllUsers = asyncHandler(async (req, res) => {
-  res.send('(GET) Read all users');
+  res.send('(GET) Admin - Read all users');
 });
 
 // @desc  Read/Get user by Id.
 // @route  GET /api/users/:id
 // @access  Private (Admin)
 const getUserById = asyncHandler(async (req, res) => {
-  res.send('(GET) Read User by Id');
+  res.send('(GET) Admin - Read User by Id');
 });
 
 // @desc  Update user by Id.
 // @route  PUT /api/users/:id
 // @access  Private (Admin)
 const updateUserById = asyncHandler(async (req, res) => {
-  res.send('(PUT) Update User by Id');
+  res.send('(PUT) Admin - Update User by Id');
 });
 
 // @desc  Delete a user
 // @route  DELETE /api/users/:id
 // @access  Private (Admin)
 const deleteUserById = asyncHandler(async (req, res) => {
-  res.send('(DELETE) User by Id');
+  res.send('(DELETE) Admin - Delete User by Id');
 });
 
 export {
@@ -72,6 +79,7 @@ export {
   logoutUser,
   getUserProfile,
   updateUserProfile,
+  createUser,
   getAllUsers,
   getUserById,
   updateUserById,
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -7,16 +7,17 @@ import {
   logoutUser,
   getUserProfile,
   updateUserProfile,
+  createUser,
   getAllUsers,
   getUserById,
   updateUserById,
   deleteUserById,
 } from '../controllers/userController.js'; // file extension needed
 
-router.route('/').post(signupUser).get(getAllUsers);
+router.route('/').post(signupUser).post(createUser).get(getAllUsers);
 router.post('/logout', logoutUser);
 router.post('/login', authUser);
-router.route('/userProfile').get(getUserProfile).put(updateUserProfile);
+router.route('/user').get(getUserProfile).put(updateUserProfile);
 router.route('/:id').get(getUserById).put(updateUserById).delete(deleteUserById);
 
 export default router;
