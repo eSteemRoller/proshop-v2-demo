3325f801b3256b8521b9698e4621b83bcc6b32dc
Jason <jasondoroughonfb@live.com>
2026-05-11 22:48:23 -0700

    157: User update.

--- backend/data/users.js
+++ backend/data/users.js
@@ -37,6 +37,12 @@ const users = [
         lastName: 'Jacob Jingleheimer Schmidt',
         password: bcrypt.hashSync('12345678', 10),
         primaryEmail: 'johnjjschmidt@email.com',
+    },
+    {
+        firstName: 'John',
+        lastName: 'Jacob Jingleheimer Schmidt',
+        password: bcrypt.hashSync('12345678', 10),
+        primaryEmail: 'johnjjschmidt@email.com',
     }
 ];
 
