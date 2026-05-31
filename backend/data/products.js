8e062298de9e5d33a032fbfa2da47c28e2c0f487
Jason <jasondoroughonfb@live.com>
2026-05-30 23:30:25 -0700

    174: Product update.

--- backend/data/products.js
+++ backend/data/products.js
@@ -48,6 +48,18 @@ const products = [
     numReviews: 12,
   },
   {
+    name: 'Cannon EOS 80D DSLR Camera',
+    image: '/images/camera.jpg',
+    description:
+      'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems and an intuitive design',
+    brand: 'Cannon',
+    category: 'Electronics',
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
     name: 'Sony Playstation 4 Pro White Version',
     image: '/images/playstation.jpg',
     description:
